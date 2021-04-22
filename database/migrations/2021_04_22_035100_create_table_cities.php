b4f352aa28f7d201ba866f504a8327f4d9f42fe8
devYamil <knt_277@hotmail.es>
2021-04-21 23:55:21 -0400

    Migracion countries, states, and cities

---
+++ database/migrations/2021_04_22_035100_create_table_cities.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_cities', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_cities');
+    }
+}
